eefc413fe503c846beb7ec261a1b5654fc7b8473
Stepan4o <stepan4oranec@gmail.com>
2024-03-11 01:14:28 +0200

    Понять что так не работает и вернуть обратно)0) (shareit-server)

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -2,8 +2,8 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -11,31 +11,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleExceptions(
-            final Exception exception
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyExistsException(
+            final AlreadyExistException exception
     ) {
-        HttpStatus status;
-        String errorMessage = exception.getMessage();
+        log.error(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
 
-        if (exception instanceof AlreadyExistException) {
-            log.error(errorMessage);
-            status = HttpStatus.CONFLICT;
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(
+            final NotFoundException exception
+    ) {
+        log.error(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
 
-        } else if (exception instanceof NotFoundException) {
-            log.error(errorMessage);
-            status = HttpStatus.NOT_FOUND;
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleAccessDeniedException(
+            final AccessDeniedException exception
+    ) {
+        log.error(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
 
-        } else if (exception instanceof AccessDeniedException) {
-            log.error(errorMessage);
-            status = HttpStatus.FORBIDDEN;
-
-        } else if (exception instanceof NotAvailableException) {
-            log.error(errorMessage);
-            status = HttpStatus.BAD_REQUEST;
-        } else {
-            log.error("Внутренняя ошибка сервера");
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-        return new ResponseEntity<>(new ErrorResponse(errorMessage), status);
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleNotAvailableException(
+            final NotAvailableException exception
+    ) {
+        log.error(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
     }
 }
